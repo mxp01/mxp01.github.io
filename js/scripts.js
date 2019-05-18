27b340fff8a84b2dc58451cf4a1a493eb8ae1a23
mikeparas <michael.paras@outlook.com>
2019-05-18 19:50:57 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -235,29 +235,29 @@ $(document).ready(function () {
 
 //test//
 
-var count=1;
-$("#btn").click(function(){
-  
-  $("#container").append(addNewRow(count));
-  count++;
-});
 
-function addNewRow(count){
-  var newrow='<div class="row">'+
-    '<div class="col-md-4">'+
-        '<div class="form-group label-floating">'+
-            '<label class="control-label">Act '+count+'</label>'+
-            '<input type="text" class="form-control" v-model="act" >'+
-        '</div>'+
-    '</div>'+
-    '<div class="col-md-4">'+
-        '<div class="form-group label-floating">'+
-            '<label class="control-label">Section '+count+'</label>'+
-            '<input type="text" class="form-control" v-model="section">'+
-        '</div>'+
-    '</div>'+    
-'</div>';
-  return newrow;
+    add-user();
+    
+
+function add-user() {
+    $(document).ready(function () {
+        var maxField = 10; //Input fields increment limitation
+        var addButton = $('.add_button'); //Add button selector
+        var wrapper = $('.field_wrapper'); //Input field wrapper
+        var fieldHTML = '<div><input type="text" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button" title="Remove field"><img src="remove-icon.png"/></a></div>'; //New input field html 
+        var x = 1; //Initial field counter is 1
+        $(addButton).click(function () {
+            if (x < maxField) { //Check maximum number of input fields
+                x++; //Increment field counter
+                $(wrapper).append(fieldHTML); // Add field html
+            }
+        });
+        $(wrapper).on('click', '.remove_button', function (e) {
+            e.preventDefault();
+            $(this).parent('div').remove(); //Remove field html
+            x--;
+        });
+    });
 }
 
 //test//
