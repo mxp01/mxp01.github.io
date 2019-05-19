0d0361dc62314a8799f3b0669f27e1ef7cf82f3e
mikeparas <michael.paras@outlook.com>
2019-05-19 01:16:21 +0100

    update

--- js/scripts.js
+++ js/scripts.js
@@ -236,26 +236,6 @@ $(document).ready(function () {
 //test//
 
 
-$(function() {
-    var scntDiv = $('#p_scents');
-    var i = $('#p_scents p').size() + 1;
-    
-    $('#addScnt').on('click', function() {
-            $('<p><label for="p_scnts"><input type="text" id="p_scnt" size="20" name="p_scnt_' + i +'" value="" placeholder="Input Value" /></label> <a href="#" id="remScnt">Remove</a></p>').appendTo(scntDiv);
-            i++;
-            return false;
-    });
-    
-    $('#remScnt').on('click', function() { 
-            if( i > 2 ) {
-                    $(this).parents('p').remove();
-                    i--;
-            }
-            return false;
-    });
-});
-
-
 //test//
 
 
